4f4a4d0bf701740d3c5e6a5344a1c8f11f8a0685
Yakov5776 <yakov5776@gmail.com>
2023-04-09 19:53:44 -0400

    feat: add ConfigManager.cs

---
+++ AdvancedRobloxArchival/ConfigManager.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedRobloxArchival
+{
+    internal class ConfigManager
+    {
+        private const string ConfigFilename = "config.json";
+        private static bool ConfigInitialized = false;
+        private static JObject _settings;
+        public static JObject Settings {
+            get {
+                if (!ConfigExist()) CreateDefaultConfig();
+                else if (!ConfigInitialized) InitializeConfig();
+                return _settings;
+            }
+        }
+
+        private static void CreateDefaultConfig()
+        {
+            _settings = new JObject(new JProperty("ConfigVersion", Program.version.ToString()));
+            ConfigInitialized = true;
+        }
+
+        public static bool ConfigExist() => File.Exists(ConfigFilename);
+
+        private static void InitializeConfig()
+        {
+            _settings = JObject.Parse(File.ReadAllText(ConfigFilename));
+            ConfigInitialized = true;
+        }
+
+        public static void FlushConfig() => File.WriteAllText(ConfigFilename, _settings.ToString());
+    }
+}
