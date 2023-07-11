28fa6945c6f067a504669952ba37b4fbbeed78de
Yakov5776 <28637006+Yakov5776@users.noreply.github.com>
2023-07-11 15:42:46 -0400

    refactor: move all site dependencies to a resource

--- AdvancedRobloxArchival/Properties/Resources.Designer.cs
+++ AdvancedRobloxArchival/Properties/Resources.Designer.cs
@@ -61,6 +61,56 @@ namespace AdvancedRobloxArchival.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to .clearfix{*zoom:1}.clearfix:before,.clearfix:after{display:table;line-height:0;content:&quot;&quot;}.clearfix:after{clear:both}.hide-text{font:0/0 a;color:transparent;text-shadow:none;background-color:transparent;border:0}.input-block-level{display:block;width:100%;min-height:30px;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}audio,canvas,video{display:inline-block;*display:inline;*zoom:1}audio:not( [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string BootstrapCSS {
+            get {
+                return ResourceManager.GetString("BootstrapCSS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to !function(a){a(function(){a.support.transition=function(){var a=function(){var a=document.createElement(&quot;bootstrap&quot;),b={WebkitTransition:&quot;webkitTransitionEnd&quot;,MozTransition:&quot;transitionend&quot;,OTransition:&quot;oTransitionEnd otransitionend&quot;,transition:&quot;transitionend&quot;},c;for(c in b)if(a.style[c]!==undefined)return b[c]}();return a&amp;&amp;{end:a}}()})}(window.jQuery),!function(a){var b=function(b,c){this.options=c,this.$element=a(b).delegate(&apos;[data-dismiss=&quot;modal&quot;]&apos;,&quot;click.dismiss.modal&quot;,a.proxy(this.hide,this)),this.optio [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string BootstrapJS {
+            get {
+                return ResourceManager.GetString("BootstrapJS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///    &lt;meta charset=&quot;UTF-8&quot;&gt;
+        ///    &lt;meta http-equiv=&quot;X-UA-Compatible&quot; content=&quot;IE=edge&quot;&gt;
+        ///    &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot;&gt;
+        ///    &lt;title&gt;Web Server&lt;/title&gt;
+        ///    &lt;link rel=&quot;stylesheet&quot; href=&quot;/css/bootstrap.css&quot;&gt;
+        ///&lt;/head&gt;
+        ///&lt;body&gt;
+        ///  &lt;div class=&quot;navbar&quot;&gt;
+        ///    &lt;div class=&quot;navbar-inner&quot;&gt;
+        ///      &lt;a class=&quot;brand&quot; href=&quot;/&quot;&gt;Advanced Roblox Archival &lt;small&gt;r{revision}&lt;/small&gt;&lt;/a&gt;
+        ///      &lt;ul class=&quot;nav&quot;&gt;
+        ///        &lt;li&gt;&lt;a href=&quot;/&quot;&gt;Home&lt;/a&gt;&lt;/ [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string HomeHTML {
+            get {
+                return ResourceManager.GetString("HomeHTML", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to (function(e,t){var n,r,i=typeof t,o=e.document,a=e.location,s=e.jQuery,u=e.$,l={},c=[],p=&quot;1.9.1&quot;,f=c.concat,d=c.push,h=c.slice,g=c.indexOf,m=l.toString,y=l.hasOwnProperty,v=p.trim,b=function(e,t){return new b.fn.init(e,t,r)},x=/[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source,w=/\S+/g,T=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,N=/^(?:(&lt;[\w\W]+&gt;)[^&gt;]*|#([\w-]*))$/,C=/^&lt;(\w+)\s*\/?&gt;(?:&lt;\/\1&gt;|)$/,k=/^[\],:{}\s]*$/,E=/(?:^|:|,)(?:\s*\[)+/g,S=/\\(?:[&quot;\\\/bfnrt]|u[\da-fA-F]{4})/g,A=/&quot;[^&quot;\\\r\n]*&quot;|true|false|null|-?(?:\d+\ [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string JQueryJS {
+            get {
+                return ResourceManager.GetString("JQueryJS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to     _      _                         _   ___  ___  ___ _    _____  __              _    _          _ 
         ///   /_\  __| |_ ____ _ _ _  __ ___ __| | | _ \/ _ \| _ ) |  / _ \ \/ /  __ _ _ _ __| |_ (_)_ ____ _| |
         ///  / _ \/ _` \ V / _` | &apos; \/ _/ -_) _` | |   / (_) | _ \ |_| (_) &gt;  &lt;  / _` | &apos;_/ _| &apos; \| \ V / _` | |
